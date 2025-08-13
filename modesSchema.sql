514c7a72069ac38cdd5d8238a5a785b474987b77
Percy Jobs <rpersichilli@outlook.com>
2025-08-13 03:35:20 -0400

    sql

---
+++ modesSchema.sql
@@ -0,0 +1,8 @@
+Mode	Interval Order	Example in C Major
+Ionian	W W H W W W H	C D E F G A B C
+Dorian	W H W W W H W	D E F G A B C D
+Phrygian	H W W W H W W	E F G A B C D E
+Lydian	W W W H W W H	F G A B C D E F
+Mixolydian	W W H W W H W	G A B C D E F G
+Aeolian	W H W W H W W	A B C D E F G A
+Locrian	H W W H W W W	B C D E F G A B
